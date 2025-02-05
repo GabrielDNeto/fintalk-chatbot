dae2cce643d12e156dc109cd4b6b2204a29a721c
Gabriel Neto <gneto@viceri.com.br>
2025-02-05 13:35:07 -0300

    fix: mobile paddings

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Banner from "./components/Banner";
 
 function App() {
   return (
-    <div className="w-full h-full py-8 dark:bg-[url('./assets/banner-img-background-effect.svg')] bg-no-repeat bg-cover bg-left">
+    <div className="w-full h-full py-4 md:py-8 dark:bg-[url('./assets/banner-img-background-effect.svg')] bg-no-repeat bg-cover bg-left">
       <div className="container mx-auto h-full">
         <Banner />
       </div>
--- src/pages/Chatbot/index.tsx
+++ src/pages/Chatbot/index.tsx
@@ -2,7 +2,7 @@ import Chat from "../../components/Chat";
 
 const Chatbot = () => {
   return (
-    <div className="w-full h-full py-8 flex flex-col items-center justify-center">
+    <div className="w-full h-full flex flex-col items-center justify-center">
       <div className="w-full max-w-[48rem] h-full p-4 flex flex-col justify-center">
         <Chat />
       </div>
